c76eb3a9be3e05762f4ace6a7d953600a873d536
Isaac Webb <6345267+isaac-webb@users.noreply.github.com>
2018-06-10 15:54:06 -0400

    Add basic properties and their unit tests

--- Sources/Complex/Complex.swift
+++ Sources/Complex/Complex.swift
@@ -1,3 +1,50 @@
-struct complex {
-    var text = "Hello, World!"
+//
+//  Complex.swift
+//
+//  Created by Isaac Webb on 5/25/18.
+//  Copyright © 2018 Isaac Webb. All rights reserved.
+//
+
+import Foundation
+
+public struct Complex {
+    // Stored properties
+    public var real, imag: Double
+    
+    // Computed Properties
+    var magnitude: Double {
+        get {
+            // Get the complex number's magnitude
+            return sqrt(real * real + imag * imag)
+        }
+        set {
+            // Capture the number's current polar angle and ensure no arithmetic errors will occur
+            let angle = abs(self.angle) <= Double.pi ? self.angle : 0
+            real = newValue * cos(angle)
+            imag = newValue * sin(angle)
+        }
+    }
+    
+    var angle: Double {
+        get {
+            let angle = atan(imag / real)
+            return abs(angle) <= Double.pi ? angle : 0
+        }
+        set {
+            let magnitude = self.magnitude
+            real = magnitude * cos(newValue)
+            imag = magnitude * sin(newValue)
+        }
+    }
+    
+    // Default constructor
+    public init(real: Double = 0, imag: Double = 0) {
+        self.real = real
+        self.imag = imag
+    }
+    
+    // Allow creation of complex numbers using polar coordinates
+    public init(magnitude: Double, angle: Double) {
+        self.init(real: magnitude * cos(angle), imag: magnitude * sin(angle))
+    }
 }
--- Tests/ComplexTests/ComplexTests.swift
+++ Tests/ComplexTests/ComplexTests.swift
@@ -2,15 +2,77 @@ import XCTest
 @testable import Complex
 
 final class ComplexTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Complex().text, "Hello, World!")
+    let tolerance = 1e-5
+    
+    // Test the default constructor for a complex number
+    func testRealImagConstructor() {
+        let z = Complex(real: 1, imag: 2)
+        XCTAssertEqual(z.real, 1)
+        XCTAssertEqual(z.imag, 2)
     }
-
-
+    
+    // Test the polar constructor for a complex number
+    func testPolarConstructor() {
+        let z = Complex(magnitude: 1, angle: Double.pi / 3)
+        XCTAssertEqual(z.real, 0.5, accuracy: tolerance)
+        XCTAssertEqual(z.imag, sqrt(3) / 2, accuracy: tolerance)
+    }
+    
+    // Test the computed magnitude property getter
+    func testMagnitudePropertyGetter() {
+        var z = Complex(real: 0, imag: 0)
+        XCTAssertEqual(z.magnitude, 0, accuracy: tolerance)
+        z.real = 1
+        XCTAssertEqual(z.magnitude, 1, accuracy: tolerance)
+        z.imag = 1
+        XCTAssertEqual(z.magnitude, sqrt(2), accuracy: tolerance)
+    }
+    
+    // Test the computed magnitude property setter
+    func testMagnitudePropertySetter() {
+        var z = Complex(real: 0, imag: 0)
+        XCTAssertEqual(z.magnitude, 0, accuracy: tolerance)
+        z.magnitude = 1
+        XCTAssertEqual(z.magnitude, 1, accuracy: tolerance)
+        XCTAssertEqual(z.real, 1, accuracy: tolerance)
+        XCTAssertEqual(z.imag, 0, accuracy: tolerance)
+        z.imag = 1
+        z.magnitude = 1
+        XCTAssertEqual(z.real, sqrt(2) / 2, accuracy: tolerance)
+        XCTAssertEqual(z.imag, sqrt(2) / 2, accuracy: tolerance)
+    }
+    
+    // Test the computed angle property getter
+    func testAnglePropertyGetter() {
+        var z = Complex(real: 0, imag: 0)
+        XCTAssertEqual(z.angle, 0, accuracy: tolerance)
+        z.real = 1
+        z.imag = 0
+        XCTAssertEqual(z.angle, 0, accuracy: tolerance)
+        z.imag = 1
+        XCTAssertEqual(z.angle, Double.pi / 4, accuracy: tolerance)
+    }
+    
+    // Test the computed magnitude property setter
+    func testAnglePropertySetter() {
+        var z = Complex(real: 1, imag: 0)
+        XCTAssertEqual(z.real, 1, accuracy: tolerance)
+        XCTAssertEqual(z.imag, 0, accuracy: tolerance)
+        z.angle = Double.pi / 3
+        XCTAssertEqual(z.magnitude, 1, accuracy: tolerance)
+        XCTAssertEqual(z.real, 0.5, accuracy: tolerance)
+        XCTAssertEqual(z.imag, sqrt(3) / 2, accuracy: tolerance)
+        z.angle = Double.pi / 2
+        XCTAssertEqual(z.real, 0, accuracy: tolerance)
+        XCTAssertEqual(z.imag, 1, accuracy: tolerance)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testRealImagConstructor", testRealImagConstructor),
+        ("testPolarConstructor", testPolarConstructor),
+        ("testMagnitudePropertyGetter", testMagnitudePropertyGetter),
+        ("testMagnitudePropertySetter", testMagnitudePropertySetter),
+        ("testAnglePropertyGetter", testAnglePropertyGetter),
+        ("testAnglePropertySetter", testAnglePropertySetter)
     ]
 }
